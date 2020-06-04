cb6c0942e433fc670de01678e30a4a1a77a9e7c9
chengri.p <chengri.piao>
2020-06-04 16:38:27 +0800

    创建模块

---
+++ basic-test/src/test/java/com/cr/basic/test/basictest/BasicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cr.basic.test.basictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
